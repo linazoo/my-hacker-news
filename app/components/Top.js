683bd1733a04983d75432d1d4e531809c3bde71e
Lina Zoo <linazooper@gmail.com>
2022-03-02 16:23:19 -0600

    keeping track of what is selected in the nav

--- app/components/Top.js
+++ app/components/Top.js
@@ -1,6 +1,20 @@
 import React from "react";
 
 export default class Top extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedPost: "Top",
+    };
+    this.updatePost = this.updatePost.bind(this);
+  }
+
+  updatePost(selectedPost) {
+    this.setState({
+      selectedPost,
+    });
+  }
   render() {
     const posts = ["Top", "New"];
 
@@ -8,7 +22,17 @@ export default class Top extends React.Component {
       <ul className="flex">
         {posts.map((post) => (
           <li key={post}>
-            <button className="btn-clear nav-link">{post}</button>
+            <button
+              className="btn-clear nav-link"
+              style={
+                post === this.state.selectedPost
+                  ? { color: "rgb(187, 46,31" }
+                  : null
+              }
+              onClick={() => this.updatePost(post)}
+            >
+              {post}
+            </button>
           </li>
         ))}
       </ul>
