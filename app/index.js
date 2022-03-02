f41f38b8c8624c7acc9f09a7f2e079c96ce23adf
Lina Zoo <linazooper@gmail.com>
2022-03-02 15:25:02 -0600

    first commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class App extends React.Component {
+  render() {
+    return <div>Hello Lina!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
