5bd8d10cdf15b0ba3a725bdd6207bacb9c3d5c64
Lina Zoo <linazooper@gmail.com>
2022-03-08 21:38:02 -0600

    comment display

--- app/components/Post.js
+++ app/components/Post.js
@@ -1,7 +1,61 @@
 import React from "react";
+import { fetchItem, fetchComments } from "../utils/api";
+import queryString from "query-string";
+import { Link } from "react-router-dom";
+import { convertTime } from "../utils/helpers";
 
 export class Post extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      comments: [],
+      loading: true,
+      title: "",
+    };
+    this.updateComments = this.updateComments.bind(this);
+  }
+
+  componentDidMount() {
+    const { id } = queryString.parse(location.search);
+    fetchItem(id).then(({ kids, title }) => {
+      this.setState({
+        title,
+      });
+      fetchComments(kids).then((data) => {
+        this.updateComments(data);
+      });
+    });
+  }
+
+  updateComments(comments) {
+    this.setState({
+      comments,
+      error: null,
+    });
+  }
   render() {
-    return <h1>hey</h1>;
+    return (
+      <React.Fragment>
+        <h1 className="header">
+          <a className="link">{this.state.title}</a>
+        </h1>
+        <ul>
+          {this.state.comments.map(({ by, text, time }) => {
+            return (
+              <li className="comment">
+                <div className="meta-info-light">
+                  <span>
+                    by <Link to={`/user?id=${by}`}>{by}</Link>
+                  </span>
+                  <span>{convertTime(time)}</span>
+                </div>
+                <p dangerouslySetInnerHTML={{ __html: text }} />
+              </li>
+            );
+          })}
+        </ul>
+      </React.Fragment>
+    );
   }
 }
