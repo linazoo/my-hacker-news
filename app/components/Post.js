710b671c5cd1fb069611b9f582f260244503878e
Lina Zoo <linazooper@gmail.com>
2022-04-18 12:46:04 -0500

    refactored index.js to use hooks but still running with previous issues on the nav bar

--- app/components/Post.js
+++ app/components/Post.js
@@ -1,65 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { fetchItem, fetchComments } from "../utils/api";
 import queryString from "query-string";
 import { Link } from "react-router-dom";
 import { convertTime } from "../utils/helpers";
 
-export class Post extends React.Component {
-  constructor(props) {
-    super(props);
+export function Post() {
+  const [comments, setComments] = useState([]);
+  const [title, setTitle] = useState("");
+  const [error, setError] = useState(null);
 
-    this.state = {
-      comments: [],
-      loading: true,
-      title: "",
-    };
-    this.updateComments = this.updateComments.bind(this);
+  function updateComments(comments) {
+    setComments(comments);
+    setError(null);
   }
 
-  componentDidMount() {
+  useEffect(() => {
     const { id } = queryString.parse(location.search);
     fetchItem(id).then(({ kids, title }) => {
-      this.setState({
-        title,
-      });
+      setTitle(title);
       fetchComments(kids)
         .then((data) => {
-          this.updateComments(data);
+          updateComments(data);
         })
         .catch((error) => {
           console.warn("Error fetching: ", error);
+          setError(error);
         });
     });
-  }
-
-  updateComments(comments) {
-    this.setState({
-      comments,
-      error: null,
-    });
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <h1 className="header">
-          <a className="link">{this.state.title}</a>
-        </h1>
-        <ul>
-          {this.state.comments.map(({ by, text, time }) => {
-            return (
-              <li key={time} className="comment">
-                <div className="meta-info-light">
-                  <span>
-                    by <Link to={`/user?id=${by}`}>{by}</Link>
-                  </span>
-                  <span>{convertTime(time)}</span>
-                </div>
-                <p dangerouslySetInnerHTML={{ __html: text }} />
-              </li>
-            );
-          })}
-        </ul>
-      </React.Fragment>
-    );
-  }
+  }, []);
+  return (
+    <>
+      <h1 className="header">
+        <a className="link">{title}</a>
+      </h1>
+      <ul>
+        {comments.map(({ by, text, time }) => {
+          return (
+            <li key={text} className="comment">
+              <div className="meta-info-light">
+                <span>
+                  by <Link to={`/user?id=${by}`}>{by}</Link>
+                </span>
+                <span>{convertTime(time)}</span>
+              </div>
+              <p dangerouslySetInnerHTML={{ __html: text }} />
+            </li>
+          );
+        })}
+        {error ? "Error fetching" : null}
+      </ul>
+    </>
+  );
 }
